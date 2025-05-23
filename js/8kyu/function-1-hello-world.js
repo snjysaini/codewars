d2b46742f0c57e97e2270557d46034c780fa3bd1
Sanjay Kumar <sanjay.developer7@gmail.com>
2025-05-23 20:13:32 +0530

    Add hello world function implementation

---
+++ js/8kyu/function-1-hello-world.js
@@ -0,0 +1,8 @@
+// Function 1 - hello world
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+
+const greet = _ => "hello world!";
