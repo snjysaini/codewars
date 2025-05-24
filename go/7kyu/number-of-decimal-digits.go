4254dd10fe21740a5cbc017fe92630540b61b7f2
Sanjay Kumar <sanjay.developer7@gmail.com>
2025-05-24 23:53:19 +0530

    Number of Decimal Digits 2

--- go/7kyu/number-of-decimal-digits.go
+++ go/7kyu/number-of-decimal-digits.go
@@ -2,12 +2,20 @@
 // https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/javascript
 
 package main
-import "strconv"
+// import "strconv"
 
+// func Digits(n uint64) int {
+// 	s2 := strconv.FormatUint(n, 10)
+// 	return len(s2)
+// }
+
+// other solutions
+
+import "fmt"
 func Digits(n uint64) int {
-	s2 := strconv.FormatUint(n, 10)
-	return len(s2)
+  return len(fmt.Sprintf("%d", n))
 }
+
 
 func main(){
 	println(Digits(123))
